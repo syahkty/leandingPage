2a0fbe5f861d7e317740d1ae82d1f870c201e1bb
syahkty <andisyahktu@gmail.com>
2023-05-23 12:08:54 +0800

    penambahan website baru

--- dist/js/script.js
+++ dist/js/script.js
@@ -42,19 +42,23 @@ const html = document.querySelector("html");
 darkToggle.addEventListener("click", function () {
   if (darkToggle.checked) {
     html.classList.add("dark");
-    localStorage.theme = 'dark'
+    localStorage.theme = "dark";
   } else {
     html.classList.remove("dark");
-    localStorage.theme = 'light'
+    localStorage.theme = "light";
   }
 });
 
 // pindah kan toggle sesuai mode
 
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  document.documentElement.classList.add('dark')
-  darkToggle.checked = true
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+  darkToggle.checked = true;
 } else {
-  document.documentElement.classList.remove('dark')
-  darkToggle.checked = false
+  document.documentElement.classList.remove("dark");
+  darkToggle.checked = false;
 }
